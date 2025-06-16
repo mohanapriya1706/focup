72aaac1f2ed2edf03f8126ca27241a7ad925f284
mohanapriya1706 <mohanapriyavasanthamani@gmail.com>
2025-06-16 16:32:11 +0530

    Removed all files after moving to kotlin-branch

--- README.md
+++
@@ -1,4 +0,0 @@
-# focup
-
-
-This is going to be mvp not a production app - The original app will be maintained privately
--- app/src/main/java/com/example/focup/FocupApplication.kt
+++
@@ -1,9 +0,0 @@
-package com.example.focup
-
-import android.app.Application
-
-class FocupApplication : Application() {
-    // Using lazy so the database and repository are only created when they're needed
-    val database by lazy { AppDatabase.getDatabase(this) }
-    val repository by lazy { TaskRepository(database.taskDao()) }
-}
